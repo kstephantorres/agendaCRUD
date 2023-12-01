3aea34b3c195c57832600e7b85ec4341fd082bc6
K Stephan Torres <kevinstephantorres@gmail.com>
2023-12-01 18:45:51 -0300

    Mostramos el contacto de contacto en la tabla al momento de crearlo

--- js/app.js
+++ js/app.js
@@ -2,17 +2,13 @@ import Contacto from "./classContacto.js";
 
 // const contactoNuevo = new Contacto(1,'nombre','apellido','mail',12345678)
 
-//variables globales
-
+//V A R I A B L E S   G L O B A L E S 
 
 // localStorage.getItem('agendaKey')  => lo trae en formato JSON
 const agenda= JSON.parse(localStorage.getItem('agendaKey')) || []
-
-
 const formNuevoContacto = document.querySelector('form')
 
-
-// funciones
+// F U N C I O N E S 
 
 const guardarLocalStorage =()=>{
     // 'agendaKey' es el nombre que va a tener nuesto objeto JSON en el local storage     
@@ -22,19 +18,43 @@ const guardarLocalStorage =()=>{
 const limpiarFormulario =()=>{
     formNuevoContacto.reset()
 }
+const cargaInicial = ()=>{
+    if(agenda.length > 0)
+    {
+        agenda.forEach((contacto,position) => {
+            crearFila(contacto,++position)
+        });
+    }
+}
 
+const crearFila=(contacto, fila)=>{
+    const tablaContacto = document.getElementById('tablaContacto')
+    tablaContacto.innerHTML += `
+        <tr>
+            <th scope="row">${fila}</th>
+            <td>${contacto.nombre}</td>
+            <td>${contacto.apellido}</td>
+            <td>${contacto.email}</td>
+            <td>${contacto.telefono}</td>
+            <td>
+            <a class="btn btn-primary" href="./pages/detalleContacto.html">Ver mas</a>
+            <button class="btn btn-warning">Editar</button>
+            <button class="btn btn-danger">Borrar</button>
+            </td>
+        </tr>
+    `
+}
 
-//logica
+//L O G I C A 
+cargaInicial()
 
 formNuevoContacto.addEventListener('submit',(event)=>{
     event.preventDefault()
 
     const inputs = document.querySelectorAll('input')
 
-    console.log(inputs)
-
     const contactoNuevo = new Contacto({
-        id:1,
+        id:crypto.randomUUID(),
         nombre: inputs[0].value,
         apellido: inputs[1].value,
         email: inputs[2].value,
@@ -44,5 +64,5 @@ formNuevoContacto.addEventListener('submit',(event)=>{
     
     guardarLocalStorage()
     limpiarFormulario()
-
+    crearFila(contactoNuevo,agenda.length)
 })
