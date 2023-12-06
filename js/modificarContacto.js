aa36c1ec0bd489176d8cc569a6814cdebc13ac59
K Stephan Torres <kevinstephantorres@gmail.com>
2023-12-06 11:02:41 -0300

    Se arreglo la funcionalidad de modificar contacto

--- js/modificarContacto.js
+++ js/modificarContacto.js
@@ -1,4 +1,4 @@
-import Contacto from "./classContacto.js"
+import { validarCantidadCaracteres, validarEmail } from "./validaciones.js"
 
 const parametroURL = new URLSearchParams(window.location.search)
 
@@ -11,6 +11,12 @@ const posicionContacto = agenda.findIndex((contacto)=>contacto.id === idContacto
 const inputs = document.querySelectorAll('input')
 
 
+const guardarLocalStorage =()=>{
+    // 'agendaKey' es el nombre que va a tener nuesto objeto JSON en el local storage     
+    localStorage.setItem('agendaKey', JSON.stringify(agenda))
+    
+}
+
 // muestro los datos del contacto en los inputs 
 inputs[0].value= agenda[posicionContacto].nombre
 inputs[1].value= agenda[posicionContacto].apellido
@@ -20,20 +26,17 @@ inputs[3].value= agenda[posicionContacto].telefono
 
 const formulario = document.querySelector('form')
 
-formulario.addEventListener('submit',()=>{
+formulario.addEventListener('submit',(event)=>{
+    event.preventDefault()  
     if(validarCantidadCaracteres(inputs[0].value, 4, 40)
        && validarCantidadCaracteres(inputs[1].value,4,40) 
-    ){
-        const contactoModificado = new Contacto({
-            id:idContacto,
-            nombre: inputs[0].value,
-            apellido: inputs[1].value,
-            email: inputs[2].value,
-            telefono: Number(inputs[3].value)})
-        
-        agenda.agenda[posicionContacto] = contactoModificado
-        
+       && validarEmail(inputs[2].value)
+    ){       
+        agenda[posicionContacto].nombre = inputs[0].value
+        agenda[posicionContacto].apellido = inputs[1].value
+        agenda[posicionContacto].email = inputs[2].value
+        agenda[posicionContacto].telefono = inputs[3].value
         guardarLocalStorage()
-        limpiarFormulario()
     }
+
 })
